37af89fd82e051ca3f1aa178a0b5b83bb0f2426f
coltongraygg <163790834+coltongraygg@users.noreply.github.com>
2024-05-27 20:49:43 +0000

    Completed data.js

--- data.js
+++ data.js
@@ -127,16 +127,7 @@ animals.push(chicken);
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-/*
 
- 3. [ ] Create a variable called `friends` and assign it to the data structure that you chose.
- 4. [ ] Take a look at the documentation for `Math.random` here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
- 5. [ ] Write a function called `getRandom` that takes our `animals` array and returns  a random `index` of the input array, using `Math.random`
- 6. [ ] Using a random index from this function that you just created, get a random animal and add its `name` to `friends`.
- 7. [ ] `console.log` `friends`.
- 8. [ ] Using **bracket notation**, add the `friends` list as a **property** also named `friends` on one of the animals in the `animals` array
- 9. [ ] `console.log` your work.
- */
 
  // Create an empty storage array for our animals friends
  var friends = []
