3c99ccc16d852ddd4b964fa50ce4f9b322dbeb29
coltongraygg <163790834+coltongraygg@users.noreply.github.com>
2024-05-27 21:00:42 +0000

    Search function complete

--- functions.js
+++ functions.js
@@ -13,8 +13,25 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
-
+/*
+## Step 1 - Search
+ 1. Open up the file `functions.js` in your editor.
+ 2. Implement a function declaration called `search` that:
+   - Takes a paramater representing an Array of `animals`.
+   - Takes a paramater representing a String, the name of an animal on which to perform a search.
+   - Looks through the `animals` Array, and returns the animal's Object if an animal with that name exists.
+   - Returns `null` if no animal with that name exists
+ 3. Use the search bar at the top of the page to make sure your function works.
+*/
+// create a search function that takes in animals array and animal name as params
+function search(animalsArr, animalName) {
+    // loop through the array, starting at 0 and stopping at the end of the animals array
+    for (var i = 0; i < animalsArr.length; i++) {
+        if (animalsArr[i].name === animalName) { // if the name at index === animalName param 
+            return animalsArr[i]; // return animal object at index
+        } 
+    } return null; // otherwise return null
+}
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
