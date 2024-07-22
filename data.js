770c1d587ff3360e8cd358b08b24a326719652e5
coltongraygg <163790834+coltongraygg@users.noreply.github.com>
2024-07-22 19:51:41 +0000

    Completed

--- data.js
+++ data.js
@@ -8,15 +8,39 @@
  * and read every instruction carefully.
  */
 
+
+
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+var animal = {};
+animal.species = 'cat';
+animal['name'] = 'puppy';
+animal.noises = [];
+
 
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+var noises = [];
+// add 'meow' to index 0 of noises
+noises[0] = 'meow';
+// add 'purr' to end of noises
+noises.push('purr');
+// add 'hiss' to beginning of array
+noises.unshift('hiss');
+// add 'meow meow' to end of noises using bracket syntax
+noises[noises.length] = 'meow meow';
+// log the length of noises
+console.log(noises.length);
+// log the last element in noises without hard coding the index
+console.log(noises.length - 1);
+// log the whole array
+console.log(noises);
+
+
 
 
 
@@ -24,7 +48,14 @@
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// use bracket syntax to assign noises property to animal
+animal['noises'] = noises;
 
+// add another noise to the noises property on animal
+animal['noises'].push('meow meow meow');
+
+// console log animal
+console.log(animal);
 
 /* *******************************************************************
  * Step 4 - Review
@@ -36,6 +67,13 @@
  * *******************************************************************
  */
 
+animal.name;
+animal['name'];
+
+noises[0];
+noises[noises.length];
+
+
 /* *******************************************************************
  * Step 5 - Take a Break!
  *
@@ -45,9 +83,34 @@
  * *******************************************************************
  */
 
+
+
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+
+// create animals array
+var animals = [];
+// push animal object to animals array
+animals.push(animal);
+// log
+console.log(animals);
+// create duck object
+var duck = {species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh']};
+// push object to animals array
+animals.push(duck);
+// log
+console.log(animals);
+// create two more objects
+var dog = {species: 'dog', name: 'Buddy', noises: ['bark', 'whine', 'growl', 'howl']};
+var rabbit = {species: 'rabbit', name: 'Thumper', noises: ['thump', 'squeak', 'sniff', 'munch']};
+// push new objects to array
+animals.push(dog);
+animals.push(rabbit);
+// log
+console.log(animals);
+console.log(animals.length);
+
 
 
 
@@ -55,7 +118,28 @@
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// use an array because this will be simple data, just a list of friends
 
+var friends = [];
+
+// create function designed to take an array
+function getRandom(array) {
+  // assign randomIndex to random index value in array
+  let randomIndex = Math.floor(Math.random() * array.length);
+  // return the randomindex of array
+  return array[randomIndex];
+}
+
+// assign randomAnimal to random index in animals array
+let randomAnimal = getRandom(animals);
+// push random index's name to friends
+friends.push(randomAnimal.name);
+// log
+console.log(friends);
+// add friends array as a property to one of the animals
+animals[0].friends = friends;
+// log that animal 
+console.log(animals[0]);
 
 /**
  * Nice work! You're done Part 1. Pat yourself on the back and
--- functions.js
+++ functions.js
@@ -9,34 +9,118 @@
  * and read every instruction carefully.
  */
 
+
+
+
+ /**
+  * I: FUNCTION RECEIVES AN ARRAY AND A STRING
+  * O: IF STRING IS FOUND IN ARRAY[I]'S NAME PROPERTY, RETURN ARRAY[I]'S OBJECT, OTHERWISE RETURN NULL
+  * C: N/A
+  * E: N/A
+  */
+
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+function search(array, string) {
+    // use found variable to return result
+    let found = null;
+    // iterate through array
+    for (let i = 0; i < array.length; i++) {
+        // search for a match
+        if (array[i].name === string) {
+            // if match is found, assign found to the match array[i]
+            found = array[i];
+        }
+    }
+    // after loop is finished return found value 
+    return found;
+    
+}
 
 
+
+/**
+ * I: FUNCTION RECEIVES AN ARRAY OF ANIMALS, A STRING NAME, AND A REPLACEMENT OBJECT
+ * O: IF STRING NAME IS FOUND IN ANIMAL'S NAME PROPERTY, FUNCTION REPLACES THE OBJECT WITH REPLACEMENT
+ * C: N/A
+ * E: N/A
+ */
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+function replace(animals, name, replacement) {
+    // iterate through array
+    for (let i = 0; i < animals.length; i++) {
+        // search for a match
+        if (animals[i].name === name) {
+            // if match is found replace 
+            animals[i] = replacement;
+        }
+    }
+}
 
+
+
+
+/**
+ * I: FUNCTION RECEIVES AN ARRAY OF ANIMALS AND A STRING NAME REPRESENTING AN ANIMALS NAME PROPERTY
+ * O: IF NAME STRING MATCHES A NAME PROPERTY IN ARRAY, FUNCTION REMOVES VALUE 
+ * C: N/A
+ * E: N/A
+ */
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+function remove(animals, name) {
+    // iterate through array
+    for (let i = 0; i < animals.length; i++) {
+        // search for a match
+        if (animals[i].name === name) {
+            // if match is found, remove current value
+            animals.splice(i, 1);
+        }
+    }
+}
 
 
+
+
+/**
+ * I: FUNCTION RECEIVES AN ARRAY OF ANIMALS AND ANIMAL REPRESENTING AN OBJECT
+ * O: FUNCTION ADDS ANIMAL OBJECT TO ANIMALS ARRAY
+ * C: ANIMAL OBJECT MUST HAVE A NAME AND SPECIES PROPERTY WITH A LENGTH > 0 AND MUST BE UNIQUE
+ * E: N/A
+ */
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function add (animals, animal) {
+    // create variable to check if animal exists in array
+    let exists = false;
+    // check if animal name and species property have a length > 0
+    if (animal['name'].length > 0 && animal['species'].length > 0) {
+        // iterate through array
+        for (let i = 0; i < animals.length; i++) {
+            // see if animal is already in array
+            if (animals[i].name === animal.name) {
+                exists = true;
+            }
+        }
+    }
+    if (exists === false) {
+        animals.push(animal);
+    }
+}
 
 
 /**
  * You did it! You're all done with Matchy!
  */
-
 
 
 //////////////////////////////////////////////////////////////////////
